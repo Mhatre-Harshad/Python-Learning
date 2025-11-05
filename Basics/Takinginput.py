a44bace6a34228c60fe4b38ad6b04f6f45e2a900
Harshad Mhatre <mhatreharshad016@gmail.com>
2025-11-05 20:27:54 +0530

    Add f-string examples below existing Taking input script

--- Basics/Takinginput.py
+++ Basics/Takinginput.py
@@ -1,4 +1,3 @@
-# input_function_demo.py
 # input function is use to take input from the user
 # Basic examples of using the input() function in Python
 
@@ -15,7 +14,30 @@ height = float(input("Enter your height in cm: "))
 print("Your height is:", height, "cm")
 
 #4 Taking multiple inputs in one line
-city, country = input("Enter your city and country  ").split()
+city, country = input("Enter your city and country with space  ").split()
 print("You live in:", city, country)
+#----------------------------------------------------------------------
+"""F-string: A formatted string literal that lets you embed 
+variables or expressions inside {} using the prefix 'f'."""
 
+#1 Simple f-string example
+student = "Harshad"
+print(f"Student name is {student}")
 
+#2  with numbers
+marks = 100
+print(f"Marks scored: {marks}")
+
+#3 with expressions
+a = 14
+b = 59
+print(f"The sum of {a} and {b} is {a + b}")
+
+#4 f-string showing formatted float
+price = 75.567
+print(f"Rounded price: {price:.2f}")
+
+#5  with multiple variables
+language = "Python"
+version = 3.12
+print(f"I am learning {language} language  version {version}")
