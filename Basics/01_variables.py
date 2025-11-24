98dfae08830879ae72dff2731a283436fd69bfaa
Harshad Mhatre <mhatreharshad016@gmail.com>
2025-11-24 19:11:07 +0530

    refactor: renamed and organized python scripts with numbered structure

---
+++ Basics/01_variables.py
@@ -0,0 +1,27 @@
+# Variables in Python
+"""A variable in Python is a container used to store data values
+ that can change during program execution."""
+# Integer variable
+age = 21
+
+# Float variable
+height = 5.9
+
+# String variable
+char="a"
+name = "Harshad"
+
+# Boolean variable (True or False)
+is_student = True
+
+# Displaying the variables
+#"print" function is used to display any data in python
+print("Name:", name)
+print("Age:", age)
+print("Height:", height)
+print("Character:",char)
+print("Student:", is_student)
+
+# Doing some operations
+next_year_age = age + 1
+print("Age next year will be:", next_year_age)
